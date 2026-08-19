7d3d0cbd02fe07abe615fa01b753acaac606f114
ashu <ashutoshmeher885@gmail.com>
2026-08-19 18:17:54 +0530

    Add input and output files for summation example
    - Created input.txt with a list of integers.
    - Created output.txt with the sum of the integers from input.txt.

---
+++ LearnC++.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //!  string and getline
+    // string s1,s2;
+    // cin >> s1 >> s2;
+    // cout << s1 << " " << s2;
+
+    // string str;
+    // getline(cin, str);
+    // cout<<str;
+
+    //! WAP that takes an input of age and prnts if you are an adult or not
+    
+    // int age;
+    // cin >> age;
+    // if (age >= 18){
+    //     cout << "you are an adult";
+    // }
+    // else{
+    //     cout << "Not an adult";
+    // }
+
+
+
+    //! WAP to ask a user to enter marks and then print the corresponding grade
+    // int marks;
+    // cin>>marks;
+    // if (marks < 25){
+    //     cout << "F";
+    // }
+    // else if(marks <= 44){
+    //     cout << "Grade: E";
+    // }
+    // else if(marks <= 49){
+    //     cout << "Grade: D";
+    // }
+    // else if(marks <= 59){
+    //     cout << "Grade: C";
+    // }
+    // else if(marks <= 79){
+    //     cout << "Grade: B";
+    // }
+    // else if(marks <= 100){
+    //     cout << "Grade: A";
+    // }
+
+
+    //! Arrays
+    // int arr[5];
+    // cin >> arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    // arr[3] += 10;
+    // cout<<arr[3];
+
+
+    //! functions
+    
+
+
+
+
+    return 0;
+}
