69cb7d42398bc78800a073949268c082c9fd57b2
ashu <ashutoshmeher885@gmail.com>
2026-08-20 00:04:04 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Patterns/Pattern5.cpp
@@ -0,0 +1,43 @@
+/*
+
+     *
+    ***
+   *****
+  *******
+ *********
+***********
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+    for (int i = 0; i < n; i++){
+
+        //* for space
+        for (int j = 0; j <= (n-i-1); j++){
+            cout << " ";
+        }
+
+        //* for stars
+        for (int j = 0; j < (2*i+1); j++){
+            
+            cout <<"*";
+        }
+
+        //* for space
+        for (int j = 0; j <= (n-i-1); j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    print(n);
+
+    return 0;
+}
