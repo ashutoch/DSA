bbe16c166a41fd147cdda9020070abc7426d2761
ashu <ashutoshmeher885@gmail.com>
2026-08-20 11:16:35 +0530

    Fix output formatting in Pattern11 and update executable

--- Patterns/Pattern11.cpp
+++ Patterns/Pattern11.cpp
@@ -16,7 +16,7 @@ void print(int n){
     int num = 1;
     for (int i = 1 ; i <= n; i++){
         for (int j = 1; j <=i; j++){
-            cout << num << "\t";
+            cout << num << " ";
             num += 1;
         }
         cout << endl;
