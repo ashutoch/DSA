532aa3651518fa1096d3fa71095d39ae6aece6cf
ashu <ashutoshmeher885@gmail.com>
2026-08-20 11:21:07 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Patterns/Pattern12.cpp
@@ -0,0 +1,33 @@
+/*
+
+A
+AB
+ABC
+ABCD
+ABCDE
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+    for (int i = 0; i < n; i++){
+        for (char j = 'A'; j <='A'+i; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++){
+        int n;
+        cin >> n;
+        print(n);
+    }
+
+    return 0;
+}
