69abba1824d0df779dc66ef951ead256d0b81e10
ashu <ashutoshmeher885@gmail.com>
2026-08-23 00:36:34 +0530

    Refactor prime number check to improve logic and remove commented-out code

--- Basic Maths Problems/Problem6.cpp
+++ Basic Maths Problems/Problem6.cpp
@@ -4,16 +4,28 @@
 using namespace std;
 
 void prime(int n){
-    int count = 1;
-    for (int i = 2; i < n; i++) {
-        if ( n % i == 0) {
+    // int count = 1;
+    // for (int i = 2; i < n; i++) {
+    //     if ( n % i == 0) {
+    //         count++;
+    //     }
+    // }
+
+    // if (count > 1) {
+    //     cout << "Its not a prime number";
+    // }
+    // else cout << "Its a prime number";
+
+    int count = 0;
+    for (int i = 1; i < sqrt(n); i++){
+        if(n % i == 0){
             count++;
+            if( n/i != i){
+                count++;
+            }
         }
     }
-
-    if (count > 1) {
-        cout << "Its not a prime number";
-    }
+    if ( count > 2) cout << "Its not a prime number";
     else cout << "Its a prime number";
 }
 
