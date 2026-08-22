54f225b15647f02733daa46316e150e914969ccb
ashu <ashutoshmeher885@gmail.com>
2026-08-23 00:18:25 +0530

    Refactor divisor function to remove commented-out code and improve clarity

--- Basic Maths Problems/Problem5.cpp
+++ Basic Maths Problems/Problem5.cpp
@@ -4,9 +4,18 @@
 using namespace std;
 
 void divisor(int n){
-    for (int i = 1; i <= n; i++){
-        if(n % i == 0) {
+    // for (int i = 1; i <= n; i++){
+    //     if(n % i == 0) {
+    //         cout << i << ", ";
+    //     }
+    // }
+
+    for (int i = 1; i <= sqrt(n); i++) {
+        if( n % i == 0) {
             cout << i << ", ";
+            if ( (n / i) != i) {
+                cout << (n / i) << ", ";
+            }
         }
     }
 }
