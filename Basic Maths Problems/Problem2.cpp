a0c93826ebdfaf4ed6f59e53973d51816eef5c7a
ashu <ashutoshmeher885@gmail.com>
2026-08-22 20:32:21 +0530

    Fix reverse function condition to ensure correct number reversal

--- Basic Maths Problems/Problem2.cpp
+++ Basic Maths Problems/Problem2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int rev(int n){
     int num, revNum = 0;
     cout << "Reverse Order: ";
-    while(n > 0){
+    while(n != 0){
         num = n % 10;
         n = n / 10;
         revNum = (revNum * 10) + num;
@@ -19,5 +19,5 @@ int main(){
     int n;
     cin >> n;
     rev(n);
-
+    return 0;
 }
