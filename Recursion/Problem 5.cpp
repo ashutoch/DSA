6374846007d88c33d1ea09ccfbb8368b56ba0305
ashu <ashutoshmeher885@gmail.com>
2026-08-23 12:26:31 +0530

    Refactor sum calculation to use tail recursion

--- Recursion/Problem 5.cpp
+++ Recursion/Problem 5.cpp
@@ -3,19 +3,29 @@
 # include <bits/stdc++.h>
 using namespace std;
 
-int print(int n) {
-    if (n == 0) return 0;
+// int print(int n) {
+//     if (n == 0) return 0;
 
-    return n + print(n - 1);
+//     return n + print(n - 1);
+// }
+
+int print(int i, int sum) {
+    if (i < 1) {
+        cout << sum;
+        return 0;
+    }
+    return print(i-1, sum + i);
 }
+
 
 int main() {
     int n;
+    int sum = 0;
     cout << "Enter a number: ";
     cin >> n;
     cout << endl;
 
-    cout << "sum is: " << print(n) << endl;
-
+    // cout << "sum is: " << print(n) << endl;
+    print(n, sum);
     return 0;
 }
