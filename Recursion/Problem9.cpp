5875822925e05592fa78af029f60ce586207d631
ashu <ashutoshmeher885@gmail.com>
2026-08-26 19:38:31 +0530

    Add recursive implementation for Fibonacci series

--- Recursion/Problem9.cpp
+++ Recursion/Problem9.cpp
@@ -21,11 +21,25 @@ int fibonacci(int n) {
     return 0;
 }
 
+int fibonacciRecursive(int n) {
+    if (n == 0 ) return 0;
+    
+    // Base Case
+    if (n <= 1) {
+        return n;
+    }
+    // Recursive Call
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
 int main() {
     int n;
     cout << "Enter the number of terms: ";
     cin >> n;
     cout << endl;
+    cout << "Fibonacci Iterative: " << endl;
     fibonacci(n);
+    cout << endl << "Fibonacci Recursive:" << endl;
+    cout << fibonacciRecursive(n);
     return 0;
 }
