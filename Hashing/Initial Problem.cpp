70cc8f2a9e85c737e47668abe69345537942138a
ashu <ashutoshmeher885@gmail.com>
2026-08-26 19:56:44 +0530

    Add program to count occurrences of a number in an array

---
+++ Hashing/Initial Problem.cpp
@@ -0,0 +1,36 @@
+//! How many times does a number comes in an array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int counterTraditional(int num, const vector<int>& arr) {
+    int count = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        if(arr[i] == num) count++;
+    }
+    return count;
+}
+
+
+int main() {
+    int num, n;
+    cout << "Enter the number of elements of the array: ";
+    cin >> n;
+    vector<int> arr(n);
+
+    cout << endl << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    cout << endl << "Enter the number to count the number of occurences: ";
+    cin >> num;
+
+    int ans = counterTraditional(num, arr);
+
+    cout << endl << "The number " << num << " occurs " << ans << " times." << endl;
+
+    //* STL shortcut
+    int ansSTL = count(arr.begin(), arr.end(), num);
+    cout << "The number " << num << " occurs " << ansSTL << " times." << endl;
+
+    return 0;
+}
