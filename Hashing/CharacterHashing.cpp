30cbacdaeba574b3f74f3c8a2752b5f4d6656230
ashu <ashutoshmeher885@gmail.com>
2026-08-26 23:34:45 +0530

    Implement character counting functionality in CharacterHashing.cpp

--- Hashing/CharacterHashing.cpp
+++ Hashing/CharacterHashing.cpp
@@ -1,2 +1,26 @@
 //! Counting the number of characters
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int hashing(char c, string s) {
+    int count = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == c) count++;
+    }
+    cout << "The number of times \"" << c << "\" occurs is: " << count;
+    return 0;
+}
+
+
+int main() {
+    string s;
+    cin >> s;
+
+    char c;
+    cout << "Enter the character to be searched for number of occurences: ";
+    cin >> c;
+
+    hashing(c, s);
+    return 0;
+}
