6f0414743270b2efbbf58a35168d031fe5b483a6
ashu <ashutoshmeher885@gmail.com>
2026-09-05 20:49:05 +0530

    Add implementation to remove duplicates from a sorted array

--- Arrays/RemovingDuplicatesInArray.cpp
+++ Arrays/RemovingDuplicatesInArray.cpp
@@ -1,1 +1,48 @@
-//!
+//! Removing duplicates from a sorted array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int sorted(vector<int>& a, int n) {
+    for (int i = 1; i < n; i++) {
+        if (a[i] >= a[i-1]){
+            // return true;
+        }
+
+        else return false;
+    }
+    return true;
+}
+
+int duplicate(vector<int>& a, int n) {
+    
+    int i = 0;
+        for (int j = 1; j < n; j++) {
+            if(a[j] != a[i]) {
+                a[i+1] = a[j];
+                i++;
+            }
+        }
+    return i+1;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> a(n);
+
+    cout << "Enter the elements of the sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    if (sorted(a, n)) {
+        cout << "There are " << duplicate(a, n) << " unique elements in the array";
+    } else {
+        cout << "Array is not sorted! \nTry Again!!!" << endl;
+    }
+
+    return 0;
+}
